93513f8e3318a4dcfa185c5b7157cafd11590fa1
commenthol <commenthol@prrtr.de>
2013-08-06 08:18:18 +0200

    brand model keys introduced

--- js/lib/device.js
+++ js/lib/device.js
@@ -1,24 +1,52 @@
 exports.Device = Device
-function Device(family) {
+function Device(family, brand, model) {
   this.family = family || 'Other';
+  this.brand = brand || null;
+  this.model = model || null;
 }
 
 Device.prototype.toString = function() {
   return this.family;
 };
 
+function replacer(options, str, matches) {
+  var i;
+  if (str) {
+    for (i in matches) {
+      if (matches[i]) {
+        str = str.replace('$' + i, matches[i]);
+      }
+      else {
+        str = str.replace('$' + i, '');
+      }
+    } 
+  }
+  else {
+    if (options.first && matches && matches[1]) {
+      str = matches[1];
+    }
+    else {
+      str = null;
+    }
+  }
+  return str;
+}
 
 exports.makeParser = function(regexes) {
   var parsers = regexes.map(function (obj) {
     var regexp = new RegExp(obj.regex),
-        deviceRep = obj.device_replacement;
+        deviceRep = obj.device_replacement,
+        brandRep = obj.brand,
+        modelRep = obj.model;
 
     function parser(str) {
       var m = str.match(regexp);
       if (!m) { return null; }
 
-      var family = deviceRep ? deviceRep.replace('$1', m[1]) : m[1];
-      return new Device(family);
+      var family = replacer({first: true}, deviceRep, m);
+      var brand = replacer({}, brandRep, m);
+      var model = replacer({first: true}, modelRep, m);
+      return new Device(family, brand, model);
     }
 
     return parser;
